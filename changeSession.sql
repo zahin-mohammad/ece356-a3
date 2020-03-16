de14294950098d8141ec50f81c0c4782feacd73a
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-16 15:12:57 -0400

    done question 1

--- changeSession.sql
+++ changeSession.sql
@@ -1,10 +1,12 @@
+SET autocommit = 0;
+
 SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-select Enrollment from Offering where courseID='ECE356';
+BEGIN;select Enrollment from Offering where courseID='ECE356';COMMIT;
 SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
-select Enrollment from Offering where courseID='ECE356';
+BEGIN;select Enrollment from Offering where courseID='ECE356';COMMIT;
 SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
-select Enrollment from Offering where courseID='ECE356';
+BEGIN;select Enrollment from Offering where courseID='ECE356';COMMIT;
 SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-select Enrollment from Offering where courseID='ECE356';
+BEGIN;select Enrollment from Offering where courseID='ECE356';COMMIT;
 
 -- show variables like 'transaction_isolation';
--- inst1.sql
+++ inst1.sql
@@ -1,3 +1,7 @@
+SET autocommit = 0;
+
+SET SESSION TRANSACTION ISOLATION LEVEL [Level];
+
 BEGIN;
 update Offering set Enrollment = Enrollment - 20 where courseID="ECE356" and section=2 and termCode=1191;
 update Offering set Enrollment = Enrollment + 20 where courseID="ECE356" and section=2 and termCode=1191;
