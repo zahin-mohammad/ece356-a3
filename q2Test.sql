d5cf4cc4c51832e5cab3b939ef965117de8dd63e
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 12:05:58 -0400

    add error code

--- q2Test.sql
+++ q2Test.sql
@@ -3,13 +3,13 @@ use uni;
 source q2.sql;
 
 -- Should pass
-call tryEnrollment('ECE356', 1, 2, 1191, 4);
+call tryEnrollment('ECE356', 1, 2, 1191, 4, @errorCode);
 -- should pass
-call tryEnrollment('ECE356', 1, 2, 1191, 14);
+call tryEnrollment('ECE356', 1, 2, 1191, 14, @errorCode);
 -- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 20)
+call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode)
 -- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 100);
+call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
 -- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 100);
+call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
 
