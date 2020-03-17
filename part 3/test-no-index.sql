1ddb2ab1e43d40932545777ffa9330b2c92b1b51
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 18:01:27 -0400

    no keys

---
+++ part 3/test-no-index.sql
@@ -0,0 +1,5 @@
+source lahman2016-no-keys.sql;
+use `baseball2016`;
+INSERT INTO performance_schema.setup_objects VALUES ('EVENT','baseball2016','%','YES','YES');
+
+source test.sql;
--- part 3/test-primary.sql
+++ part 3/test-primary.sql
@@ -1,4 +1,5 @@
 source lahman2016-no-foreign-keys.sql;
-INSERT INTO setup_objects VALUES (’EVENT’,’baseball2016’,’%’,’YES’,’YES’);
+use `baseball2016`;
+INSERT INTO performance_schema.setup_objects VALUES ('EVENT','baseball2016','%','YES','YES');
 
 source test.sql;
