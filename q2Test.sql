ca74352d75cad55af461cec38dcb822300c589f1
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 12:15:06 -0400

    print error code

--- q2Test.sql
+++ q2Test.sql
@@ -4,12 +4,17 @@ source q2.sql;
 
 -- Should pass
 call tryEnrollment('ECE356', 1, 2, 1191, 4, @errorCode);
+SELECT @errorCode;
 -- should pass
 call tryEnrollment('ECE356', 1, 2, 1191, 14, @errorCode);
+SELECT @errorCode;
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
+SELECT @errorCode;
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
+SELECT @errorCode;
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
+SELECT @errorCode;
 
