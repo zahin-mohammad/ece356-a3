17d1846e6b8d91c2b86a843589f43e4f495dedf2
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 18:52:24 -0400

    all keys

--- part 3/test-primary-foreign-index.sql
+++ part 3/test-primary-foreign-index.sql
@@ -1,5 +1,6 @@
-source lahman2016.sql;
+source lahman2016-all-keys.sql;
 use `baseball2016`;
+
 INSERT INTO performance_schema.setup_objects VALUES ('EVENT','baseball2016','%','YES','YES');
 
 source test.sql;
