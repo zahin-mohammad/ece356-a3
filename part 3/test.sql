ef2c7c5fca06b2a750fc631e9128299bf9d0ef24
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 17:34:59 -0400

    change to ms

--- part 3/test.sql
+++ part 3/test.sql
@@ -3,42 +3,42 @@ source query1x5.sql;
 
 SELECT 
     AVG(TIMER_WAIT)/1000000 as 'AverageRunTime',
-    min(TIMER_WAIT) as 'SmallestRunTime',
-    max(TIMER_WAIT) as 'LargestRunTime'
+    min(TIMER_WAIT)/1000000 as 'SmallestRunTime',
+    max(TIMER_WAIT)/1000000 as 'LargestRunTime'
 FROM performance_schema.events_transactions_history;
 
-/* CALL sys.ps_truncate_all_tables(FALSE);
+CALL sys.ps_truncate_all_tables(FALSE);
 source query2x5.sql;
 
 SELECT 
-    sum(TIMER_END-TIMER_START)/5 as 'AverageRunTime',
-    min(TIMER_END-TIMER_START) as 'SmallestRunTime',
-    max(TIMER_END-TIMER_START) as 'LargestRunTime'
+    AVG(TIMER_WAIT)/1000000 as 'AverageRunTime',
+    min(TIMER_WAIT)/1000000 as 'SmallestRunTime',
+    max(TIMER_WAIT)/1000000 as 'LargestRunTime'
 FROM performance_schema.events_transactions_history;
 
 CALL sys.ps_truncate_all_tables(FALSE);
 source query3x5.sql;
 
 SELECT 
-    sum(TIMER_END-TIMER_START)/5 as 'AverageRunTime',
-    min(TIMER_END-TIMER_START) as 'SmallestRunTime',
-    max(TIMER_END-TIMER_START) as 'LargestRunTime'
+    AVG(TIMER_WAIT)/1000000 as 'AverageRunTime',
+    min(TIMER_WAIT)/1000000 as 'SmallestRunTime',
+    max(TIMER_WAIT)/1000000 as 'LargestRunTime'
 FROM performance_schema.events_transactions_history;
 
 CALL sys.ps_truncate_all_tables(FALSE);
 source query4x5.sql;
 
 SELECT 
-    sum(TIMER_END-TIMER_START)/5 as 'AverageRunTime',
-    min(TIMER_END-TIMER_START) as 'SmallestRunTime',
-    max(TIMER_END-TIMER_START) as 'LargestRunTime'
+    AVG(TIMER_WAIT)/1000000 as 'AverageRunTime',
+    min(TIMER_WAIT)/1000000 as 'SmallestRunTime',
+    max(TIMER_WAIT)/1000000 as 'LargestRunTime'
 FROM performance_schema.events_transactions_history;
 
 CALL sys.ps_truncate_all_tables(FALSE);
 source query5x5.sql;
 
 SELECT 
-    sum(TIMER_END-TIMER_START)/5 as 'AverageRunTime',
-    min(TIMER_END-TIMER_START) as 'SmallestRunTime',
-    max(TIMER_END-TIMER_START) as 'LargestRunTime'
-FROM performance_schema.events_transactions_history; */
+    AVG(TIMER_WAIT)/1000000 as 'AverageRunTime',
+    min(TIMER_WAIT)/1000000 as 'SmallestRunTime',
+    max(TIMER_WAIT)/1000000 as 'LargestRunTime'
+FROM performance_schema.events_transactions_history;
