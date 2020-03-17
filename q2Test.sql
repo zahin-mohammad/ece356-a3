49418d6d971a31cf87d2e088b99e838c20c8a007
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 12:13:56 -0400

    missing ;

--- q2Test.sql
+++ q2Test.sql
@@ -7,7 +7,7 @@ call tryEnrollment('ECE356', 1, 2, 1191, 4, @errorCode);
 -- should pass
 call tryEnrollment('ECE356', 1, 2, 1191, 14, @errorCode);
 -- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode)
+call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
 -- should fail
