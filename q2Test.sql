a271c13ac38ac3aebe460938262115046c88777f
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 12:03:51 -0400

    uncomment tests

--- q2Test.sql
+++ q2Test.sql
@@ -2,14 +2,14 @@ use uni;
 
 source q2.sql;
 
--- -- Should pass
--- call tryEnrollment('ECE356', 1, 2, 1191, 4);
--- -- should pass
--- call tryEnrollment('ECE356', 1, 2, 14);
--- -- should fail
--- call tryEnrollment('ECE356', 1, 2, 20)
--- -- should fail
--- call tryEnrollment('ECE356', 1, 2, 1191, 100);
--- -- should fail
--- call tryEnrollment('ECE356', 1, 2, 1191, 100);
+-- Should pass
+call tryEnrollment('ECE356', 1, 2, 1191, 4);
+-- should pass
+call tryEnrollment('ECE356', 1, 2, 14);
+-- should fail
+call tryEnrollment('ECE356', 1, 2, 20)
+-- should fail
+call tryEnrollment('ECE356', 1, 2, 1191, 100);
+-- should fail
+call tryEnrollment('ECE356', 1, 2, 1191, 100);
 
