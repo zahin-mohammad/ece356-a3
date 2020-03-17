d637709e7d236bdd18d2f3e4766460bf9d039fea
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 13:02:42 -0400

    put test in a transaction

--- q2Test.sql
+++ q2Test.sql
@@ -3,22 +3,28 @@ use uni;
 source q2.sql;
 
 -- Should pass
+START TRANSACTION;
 call tryEnrollment('ECE356', 1, 2, 1191, 4, @errorCode);
 SELECT @errorCode;
+ROLLBACK;
+COMMIT;
 
 -- should pass
+START TRANSACTION;
 call tryEnrollment('ECE356', 1, 2, 1191, 14, @errorCode);
 SELECT @errorCode;
+ROLLBACK;
+COMMIT;
 
--- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
-SELECT @errorCode;
+-- -- should fail
+-- call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
+-- SELECT @errorCode;
 
--- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
-SELECT @errorCode;
+-- -- should fail
+-- call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
+-- SELECT @errorCode;
 
--- should fail
-call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
-SELECT @errorCode;
+-- -- should fail
+-- call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
+-- SELECT @errorCode;
 
