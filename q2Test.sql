1517a46c33135aa577b34aa5063efde5547b7ddd
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 12:04:53 -0400

    wrong number of params

--- q2Test.sql
+++ q2Test.sql
@@ -5,9 +5,9 @@ source q2.sql;
 -- Should pass
 call tryEnrollment('ECE356', 1, 2, 1191, 4);
 -- should pass
-call tryEnrollment('ECE356', 1, 2, 14);
+call tryEnrollment('ECE356', 1, 2, 1191, 14);
 -- should fail
-call tryEnrollment('ECE356', 1, 2, 20)
+call tryEnrollment('ECE356', 1, 2, 1191, 20)
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 100);
 -- should fail
