4136db9fdf901d43bf87f48bbbff6522aa700732
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 14:32:15 -0400

    fck this shit

--- q2Test.sql
+++ q2Test.sql
@@ -19,16 +19,34 @@ source q2.sql;
 call tryEnrollment('ECE356', 1, 2, 1191, 14, @errorCode);
 SELECT @errorCode;
 
--- -- should fail
--- call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
--- SELECT @errorCode;
+DROP DATABASE IF EXISTS unitest;
+CREATE DATABASE unitest;
+use unitest;
+source createUni.sql;
+set autocommit=0;
+source q2.sql;
+-- should fail
+call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
+SELECT @errorCode;
 
--- -- should fail
--- call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
--- SELECT @errorCode;
+DROP DATABASE IF EXISTS unitest;
+CREATE DATABASE unitest;
+use unitest;
+source createUni.sql;
+set autocommit=0;
+source q2.sql;
+-- should fail
+call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
+SELECT @errorCode;
 
--- -- should fail
--- call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
--- SELECT @errorCode;
+DROP DATABASE IF EXISTS unitest;
+CREATE DATABASE unitest;
+use unitest;
+source createUni.sql;
+set autocommit=0;
+source q2.sql;
+-- should fail
+call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
+SELECT @errorCode;
 
 set autocommit=1;
