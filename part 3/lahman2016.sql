0c2c0007bfa29e92147728472b7f0b490bac504e
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 17:25:24 -0400

    remove batting

--- part 3/lahman2016.sql
+++ part 3/lahman2016.sql
@@ -6,7 +6,6 @@ SET FOREIGN_KEY_CHECKS=0;
 
 source lahman2016-tables.sql;
 source lahman2016-data.sql;
-source lahman2016-batting.sql;
 
 INSERT INTO Schools (schoolid) (SELECT DISTINCT schoolid FROM CollegePlaying WHERE schoolid NOT IN (SELECT schoolid FROM Schools));
 INSERT INTO Master (playerid) (SELECT DISTINCT playerid FROM HallOfFame WHERE playerid NOT IN (SELECT playerid from Master));
