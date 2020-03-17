1c2f59f6219e2dd9a7a9683220dc527801059867
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 17:42:09 -0400

    change to ms

--- part 3/test-primary-foreign.sql
+++ part 3/test-primary-foreign.sql
@@ -1,5 +1,5 @@
 source lahman2016.sql;
 use `baseball2016`;
-INSERT INTO setup_objects VALUES (’EVENT’,’baseball2016’,’%’,’YES’,’YES’);
+INSERT INTO performance_schema.setup_objects VALUES (’EVENT’,’baseball2016’,’%’,’YES’,’YES’);
 
 source test.sql;
