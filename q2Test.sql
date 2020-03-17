f52b08f085402352e31ec2ad4ceb573d7831413b
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-17 14:38:24 -0400

    fck this shit

--- q2Test.sql
+++ q2Test.sql
@@ -4,10 +4,9 @@ use unitest;
 source createUni.sql;
 set autocommit=0;
 source q2.sql;
-
 -- Should pass
 call tryEnrollment('ECE356', 1, 2, 1191, 0, @errorCode);
-SELECT @errorCode;
+SELECT CONCAT('EXPECTED 0 GOT ', @errorCode) AS 'result';
 
 DROP DATABASE IF EXISTS unitest;
 CREATE DATABASE unitest;
@@ -17,7 +16,7 @@ set autocommit=0;
 source q2.sql;
 -- should pass
 call tryEnrollment('ECE356', 1, 2, 1191, 14, @errorCode);
-SELECT @errorCode;
+SELECT CONCAT('EXPECTED 0 GOT ', @errorCode) AS 'result';
 
 DROP DATABASE IF EXISTS unitest;
 CREATE DATABASE unitest;
@@ -27,7 +26,7 @@ set autocommit=0;
 source q2.sql;
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 20, @errorCode);
-SELECT CONCAT('EXPECTED -3 GOT ', @errorCode);
+SELECT CONCAT('EXPECTED -3 GOT ', @errorCode) AS 'result';
 
 DROP DATABASE IF EXISTS unitest;
 CREATE DATABASE unitest;
@@ -37,6 +36,6 @@ set autocommit=0;
 source q2.sql;
 -- should fail
 call tryEnrollment('ECE356', 1, 2, 1191, 100, @errorCode);
-SELECT CONCAT('EXPECTED -2 GOT ', @errorCode);
+SELECT CONCAT('EXPECTED -2 GOT ', @errorCode) AS 'result';
 
 set autocommit=1;
